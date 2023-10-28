aa8c52a7c838f66be90ab58b801849373b4e0c5e
donatasadamonis <dadamonis@gsma.com>
2023-10-29 00:24:08 +0100

    touch

--- leo_midi/leo_midi.ino
+++ leo_midi/leo_midi.ino
@@ -1,6 +1,7 @@
 /*
   Based on Sketch built by Gustavo Silveira (aka Music Nerd)
   Modified by Dolce Wang
+  Modified by Ad0mA5
 
   This code is only for Arduinos that use ATmega32u4 (like Micro, Pro Micro, Leonardo...)
   Remember to also assign the correct board in the IDE (like Tools / Boards / Sparkfun AVR / Pro Micro...)
@@ -11,19 +12,16 @@
 
 
 // LIBRARY
+#include <CapacitiveSensor.h>
 
 #include "MIDIUSB.h"  
 
 // BUTTONS
-const int NButtons = 19; //***  total number of buttons
-const int buttonPin[NButtons] = {0,1,2,4,5, 6,7,8,9,10, 11,12,13,A0,A1, A2,A3,A4,A5}; //*** define Digital Pins connected from Buttons to Arduino; (ie {10, 16, 14, 15, 6, 7, 8, 9, 2, 3, 4, 5}; 12 buttons)
-const int speakerPin = 3;    
-//const int sliderIn = A5;
-                                        
-const int speakerNotes[NButtons] = {440,466,494,523,554, 587,622,659,698,740, 784,831,880,932,988, 1047,1109,1175,1245};
+const int NButtons = 1; //***  total number of buttons
+const int buttonPin[NButtons] = {13};
+//{0,1,2,4,5, 7,8,9,10,A0,A1, A2,A3,A4,A5}; //*** define Digital Pins connected from Buttons to Arduino; (ie {10, 16, 14, 15, 6, 7, 8, 9, 2, 3, 4, 5}; 12 buttons)
 int buttonCState[NButtons] = {};        // stores the button current value
 int buttonPState[NButtons] = {};        // stores the button previous value
-
       
 // debounce
 unsigned long lastDebounceTime[NButtons] = {0};  // the last time the output pin was toggled
@@ -31,9 +29,9 @@ unsigned long debounceDelay = 50;    //** the debounce time; increase if the out
 
 
 // POTENTIOMETERS
-const int NPots = 1; //*** total number of pots (knobs and faders)
+const int NPots = 4; //*** total number of pots (knobs and faders)
 
-const int potPin[NPots] = {/*A9, A8, A7, A6, A3, A2, A1, A0*/}; //*** define Analog Pins connected from Pots to Arduino; Leave nothing in the array if 0 pots {}
+const int potPin[NPots] = {A0, A1, A3, A5}; //*** define Analog Pins connected from Pots to Arduino; Leave nothing in the array if 0 pots {}
 int potCState[NPots] = {0}; // Current state of the pot; delete 0 if 0 pots
 int potPState[NPots] = {0}; // Previous state of the pot; delete 0 if 0 pots
 int potVar = 0; // Difference between the current and previous state of the pot
@@ -52,10 +50,46 @@ unsigned long timer[NPots] = {0}; // Stores the time that has elapsed since the 
 byte midiCh = 1; //* MIDI channel to be used
 byte note = 60; //* Lowest note to be used; 36 = C2; 60 = Middle C
 byte cc = 1; //* Lowest MIDI CC to be used
-int toned = 1;
 //bool connected = false;
 
+
+// CAPACCITIVE TOUCH
+const int sendPin = 12;
+const int Ncaps = 5; //*** total number of capacitive touchers
+
+const int capPin[Ncaps] = {7,8,9,10,11}; //*** define Digital Pins connected from Touchpads to Arduino; Leave nothing in the array if 0 pots {}
+const CapacitiveSensor capSensors[Ncaps] = { 
+  CapacitiveSensor(sendPin, capPin[0]),
+  CapacitiveSensor(sendPin, capPin[1]),
+  CapacitiveSensor(sendPin, capPin[2]),
+  CapacitiveSensor(sendPin, capPin[3]),
+  CapacitiveSensor(sendPin, capPin[4])
+
+};
+int capCState[Ncaps] = {0}; // Current state of the pad; delete 0 if 0 pads
+int capPState[Ncaps] = {0}; // Previous state of the pad; delete 0 if 0 pads
+//int capVar = 0; // Difference between the current and previous state of the pad
+
+//initialize capacitive sense 
+//unsigned long csSum;
+//unsigned long csSum12;
+//unsigned long csSum6;
+
+//CapacitiveSensor   cs_6_11 = CapacitiveSensor(6,11);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+//CapacitiveSensor   cs_6_12 = CapacitiveSensor(6,12);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+// To add more sensors...
+
+
+//speaker
+const int speakerPin = 3;    
+const int speakerNotes[Ncaps] = {440,466,494,523,554 //, 587,622,659,698,740, 784,831,880,932,988
+//, 1047
+//,1109,1175
+//,1245
+};
+int toned = 1;
 int startPress13 = 0;
+
 // SETUP
 void setup() {
 
@@ -66,6 +100,12 @@ void setup() {
   // Initialize buttons with pull up resistors
   for (int i = 0; i < NButtons; i++) {
     pinMode(buttonPin[i], INPUT_PULLUP);
+  }
+  // Init capacitive touch pins
+  for (int i = 0; i < Ncaps; i++) {
+ //   CapacitiveSensor   cs = CapacitiveSensor(sendPin, capPin[i]);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+   capSensors[i].set_CS_AutocaL_Millis(0xFFFFFFFF);  //Calibrate the sensor... pinMode(led, OUTPUT);
+  // capSensors[i] = cs;
   }
 
 }
@@ -82,6 +122,7 @@ void loop() {
 */
   buttons();
   potentiometers();
+  capacitiveButtons();
 
 }
 
@@ -104,14 +145,11 @@ void buttons() {
 						startPress13 = millis();
 						//tone(speakerPin, 880, 500);
 					}
-					//if(connected){
-					//}else{
 	          if(toned == 1) 
               tone(speakerPin, speakerNotes[i], 3);
 	          if(toned == 2) 
               tone(speakerPin, speakerNotes[i]);
             //toned = true;
-					//}
           // Sends the MIDI note ON 
           
          // use if using with ATmega32U4 (micro, pro micro, leonardo...)
@@ -120,11 +158,6 @@ void buttons() {
         }
         else {
 
-					//if(connected){
-					//}else{
- 					//toned = false;
-				    //noTone(speakerPin);
-					//}
 	        if(toned == 2) 
               noTone(speakerPin);
           if(buttonPin[i] == 13){
@@ -145,18 +178,18 @@ void buttons() {
 				//if(millis() - startPress13 > 2500) startPress13 = 0;
       }
     }
-				if(buttonPin[i] == 13 && startPress13 >= 1){
-          int dt = millis() - startPress13;
- 					if(dt  >= 5000){
-          //tone(speakerPin, 440, dt);
-					//delay(1000);
-          tone(speakerPin, 440, 10);
-          tone(speakerPin, 440, 10);
-					toned = toned+1;
-					if(toned >= 3) toned = 0;
-					startPress13 = 0;
-					}
-				}
+		if(buttonPin[i] == 13 && startPress13 >= 1){
+			int dt = millis() - startPress13;
+			if(dt  >= 5000){
+			//tone(speakerPin, 440, dt);
+			//delay(1000);
+			tone(speakerPin, 440, 10);
+			tone(speakerPin, 440, 10);
+			toned = toned+1;
+			if(toned >= 3) toned = 0;
+			startPress13 = 0;
+			}
+		}
   }
 }
 
@@ -219,4 +252,71 @@ void noteOff(byte channel, byte pitch, byte velocity) {
 void controlChange(byte channel, byte control, byte value) {
   midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
   MidiUSB.sendMIDI(event);
+}
+
+
+bool trash = false;
+void capacitiveButtons(){
+  for (int i = 0; i < Ncaps; i++) {
+    long cs_val = capSensors[i].capacitiveSensor(100); //a: Sensor resolution is set to 80
+
+		if (cs_val > 300) { //b: Arbitrary number
+			//csSum += cs;
+			capCState[i] += cs_val;  // read pins from arduino
+			//Serial.println(cs); 
+			if (capCState[i] >= 6000) //c: This value is the threshold, a High value means it takes longer to trigger
+			{
+        if (capPState[i] != capCState[i]) {
+
+         // use if using with ATmega32U4 (micro, pro micro, leonardo...)
+          noteOn(midiCh, note + i, 127);  // channel, note, velocity
+          MidiUSB.flush();
+
+          capPState[i] = capCState[i];
+        }
+				//trash = true;//!trash;
+				//if (trash) digitalWrite(13, HIGH);
+				//else digitalWrite(13, LOW);
+				//Serial.print("Trigger: ");
+				//Serial.println(csSum);
+				//if (csSum > 0) { csSum = 0; } //Reset
+				if(capCState[i] > 0 ) { capCState[i] = 0; } //reset		
+				capSensors[i].reset_CS_AutoCal(); //Stops readings
+
+			}else{
+
+        if (capPState[i] != capCState[i]) {
+
+         // use if using with ATmega32U4 (micro, pro micro, leonardo...)
+          // use if using with ATmega32U4 (micro, pro micro, leonardo...)
+          noteOn(midiCh, note + i, 0);  // channel, note, velocity
+          MidiUSB.flush();
+
+          capPState[i] = capCState[i];
+        }
+      }  
+		} else {
+			 capCState[i] = 0; //Timeout caused by bad readings
+		}
+  }//end for
+
+/*
+  long cs12 = cs_6_12.capacitiveSensor(100); //a: Sensor resolution is set to 80
+	if (cs12 > 300) { //b: Arbitrary number
+		csSum12 += cs12;
+		//Serial.println(cs); 
+		if (csSum12 >= 6000) //c: This value is the threshold, a High value means it takes longer to trigger
+		{
+			trash = false;//!trash;
+			if (trash) digitalWrite(13, HIGH);
+			else digitalWrite(13, LOW);
+			//Serial.print("Trigger: ");
+			//Serial.println(csSum);
+			if (csSum12 > 0) { csSum12 = 0; } //Reset
+			cs_6_12.reset_CS_AutoCal(); //Stops readings
+		}
+	} else {
+		csSum12 = 0; //Timeout caused by bad readings
+	}
+*/
 }
