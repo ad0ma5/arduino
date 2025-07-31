3f45cfd4bc977c88fa0c0d2b7ec0ea1ae9934611
donatasadamonis <dadamonis@gsma.com>
2025-07-31 18:43:53 +0100

    serial

--- nano_serial/nano_serial.ino
+++ nano_serial/nano_serial.ino
@@ -1,12 +1,15 @@
-const int numButtons = 11;
-const int buttonPins[numButtons] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+const int numButtons = 17;
+const int buttonPins[numButtons] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, A0, A1, A2, A3, A4, A5};
 bool buttonStates[numButtons];       // Current button state
 bool lastButtonStates[numButtons];   // Previous button state
 unsigned long lastDebounceTimes[numButtons]; // Last debounce time
-const unsigned long debounceDelay = 50;      // 50 ms debounce
-const int ledPin = 13;
-bool ledState = false;
+const unsigned long debounceDelay = 5;      // 50 ms debounce
+const int ledPin1 = 13;
+const int ledPin2 = 13;
+int ledPin = 13;
+bool ledState[2] = {LOW, LOW};
 char bufer[20];
+int pinState = 0;
 void setup() {
   for (int i = 0; i < numButtons; i++) {
     pinMode(buttonPins[i], INPUT_PULLUP);
@@ -14,11 +17,20 @@ void setup() {
     buttonStates[i] = HIGH;
     lastDebounceTimes[i] = 0;
   }
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  //pinMode(A7, OUTPUT);
   Serial.begin(9600);
 }
 void loop() {
   for (int i = 0; i < numButtons; i++) {
+    if (i < 11){
+      ledPin = ledPin1;
+      pinState = 0; 
+    }else{
+      ledPin = ledPin2;
+      pinState = 1;
+    }
     int reading = digitalRead(buttonPins[i]);
     // If button state changed
     if (reading != lastButtonStates[i]) {
@@ -30,12 +42,13 @@ void loop() {
         buttonStates[i] = reading;
         // If button is pressed (goes LOW)
         if (buttonStates[i] == LOW) {
-          ledState = HIGH;
-          digitalWrite(ledPin, ledState);
+          ledState[pinState] = HIGH;
+          digitalWrite(ledPin, ledState[pinState]);
           sprintf(bufer,"digital %d 0", buttonPins[i]);
           Serial.println(bufer);
         }else{
-          ledState = LOW;
+          ledState[pinState] = LOW;
+          digitalWrite(ledPin, ledState[pinState]);   // Turn LED off
           sprintf(bufer,"digital %d 1", buttonPins[i]);
           Serial.println(bufer);
         }
@@ -43,8 +56,9 @@ void loop() {
     }
     // Handle button release (rising edge) immediately, no debounce
     if (lastButtonStates[i] == LOW && reading == HIGH) {
-      ledState = false;
-      digitalWrite(ledPin, ledState);   // Turn LED off
+      ledState[pinState] = LOW;
+      digitalWrite(ledPin, ledState[pinState]);   // Turn LED off
+      sprintf(bufer,"digital %d 1", buttonPins[i]);
     }
     lastButtonStates[i] = reading;
   }
